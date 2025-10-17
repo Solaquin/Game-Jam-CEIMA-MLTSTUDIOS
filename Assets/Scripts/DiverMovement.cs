93eca70fa22dd49ad213ae915f5f48bb39787ad3
Nicolukaz <106442324+Nicolukazzz@users.noreply.github.com>
2025-10-17 18:35:24 -0500

    Create PlayerMovement

---
+++ Assets/Scripts/DiverMovement.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class DiverMovement : MonoBehaviour
+{
+    [Header("Movement Settings")]
+    [SerializeField] private float swimSpeed = 1f;
+    [SerializeField] private float acceleration = 1f;
+    [SerializeField] private float deceleration = 0.5f;
+
+    private Rigidbody2D rb;
+    private Vector2 input;
+    private Vector2 currentVelocity;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        input.x = Input.GetAxis("Horizontal");
+        input.y = Input.GetAxis("Vertical");
+        input = input.normalized;
+    }
+
+    void FixedUpdate()
+    {
+        HandleMovement();
+    }
+
+    void HandleMovement()
+    {
+       
+        Vector2 targetVelocity = input * swimSpeed;
+
+        currentVelocity = Vector2.Lerp(
+            currentVelocity,
+            targetVelocity,
+            Time.fixedDeltaTime * (input.magnitude > 0 ? acceleration : deceleration)
+        );
+
+        rb.linearVelocity = currentVelocity;
+    }
+}
