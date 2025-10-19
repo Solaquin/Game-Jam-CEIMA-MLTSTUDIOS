496c348e05a6bf77bdc20577923362c67c87f4c3
Nicolukaz <106442324+Nicolukazzz@users.noreply.github.com>
2025-10-19 00:54:08 -0500

    Update Escena Final

--- Assets/Scripts/SurfaceZone.cs
+++ Assets/Scripts/SurfaceZone.cs
@@ -5,8 +5,8 @@ public class SurfaceZone : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI pressEText;
 
-    private Vector3 baseSpawnPoint = new Vector3(0f, 33f, 0f);
-    private Vector3 waterSpawnPoint = new Vector3(9f, 30f, 0f);
+    private Vector3 baseSpawnPoint = new Vector3(-18f, 21f, 0f);
+    private Vector3 waterSpawnPoint = new Vector3(-6f, 14f, 0f);
 
     private bool isPlayerNearby = false;
     private bool isAtBase = false; 
